8748cbd686a23e5b4a0e5a1ea678f6aed22cf4b4
b3nj5m1n <b3nj4m1n@gmx.net>
2026-05-27 09:03:01 +0200

    Add install script for wallpaper

---
+++ install_wallpaper.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+WALLPAPER_LOCATION="/usr/local/share/wallpaper"
+
+cp "./blackhole-smooth-240x67.dur" "$WALLPAPER_LOCATION/blackhole.dur"
